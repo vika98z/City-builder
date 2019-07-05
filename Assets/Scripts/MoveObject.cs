7f8716bdceae16069bbb47ea0446c40e886c927c
Мялкина Виктория Олеговна <myalkina@sfedu.ru>
2019-07-06 00:34:25 +0300

    Добавлена возможность перемещать объекты

--- Assets/Scripts/MoveObject.cs
+++ Assets/Scripts/MoveObject.cs
@@ -4,18 +4,62 @@ using UnityEngine;
 
 public class MoveObject : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool _mouseState;
+
+    public Camera cam;
+
+    public GameObject Object;
+    public Vector3 screenSpace;
+    public Vector3 offset;
+
+    // Use this for initialization
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(1))
+        //Если мы в режиме стройки
+        if (Static.isBuild)
         {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hitInfo;
+                if (Object == GetClickedObject(out hitInfo))
+                {
+                    Object.GetComponent<Rigidbody>().useGravity = false;
+                    Object.GetComponent<Rigidbody>().freezeRotation = true;
+                    _mouseState = true;
+                    screenSpace = cam.WorldToScreenPoint(Object.transform.position);
+                    offset = Object.transform.position - cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z));
+                }
+            }
 
+            if (Input.GetMouseButtonUp(0))
+            {
+                _mouseState = false;
+            }
+
+            if (_mouseState)
+            {
+                var curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z);
+                var curPosition = cam.ScreenToWorldPoint(curScreenSpace) + offset;
+                Object.transform.position = curPosition;
+            }
         }
+    }
+
+    GameObject GetClickedObject(out RaycastHit hit)
+    {
+        GameObject target = null;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
+        {
+            target = hit.collider.gameObject;
+        }
+
+        return target;
     }
 }
