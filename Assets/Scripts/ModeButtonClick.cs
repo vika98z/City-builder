930901e2b96682d97f2cbf6796b1c7b262ef304e
Мялкина Виктория Олеговна <myalkina@sfedu.ru>
2019-07-06 00:52:05 +0300

    Добавлена смена камер

---
+++ Assets/Scripts/ModeButtonClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModeButtonClick : MonoBehaviour
+{
+    public Camera frontCamera;
+    public Camera topCamera;
+
+    public void BuildModeClick()
+    {
+        Static.isBuild = true;
+        frontCamera.enabled = false;
+        topCamera.enabled = true;
+    }
+
+    public void RegularModeClick()
+    {
+        Static.isBuild = false;
+        frontCamera.enabled = true;
+        topCamera.enabled = false;
+    }
+}
